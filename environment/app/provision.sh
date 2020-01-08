0b0d279895d956f58aa494304fa451c83771939c
Sam Watson <SWatson@spartaglobal.com>
2020-01-08 15:15:33 +0000

    first commit

---
+++ environment/app/provision.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install pm2
+sudo npm install pm2 -g
+
+sudo npm install ejs mongoose express
+
+# remove the default and replace with our configuration
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/ubuntu/environment/app/default /etc/nginx/sites-enabled/default
+sudo systemctl stop nginx
+sudo systemctl start nginx
+sudo nginx -t
